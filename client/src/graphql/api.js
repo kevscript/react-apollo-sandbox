6e64070faaa48f4be87fd740e33f737a4622842b
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-01-17 18:16:20 +0100

    created book completion toggler HOOK + updated all queries with completed property

--- client/src/graphql/api.js
+++ client/src/graphql/api.js
@@ -2,24 +2,31 @@ import { gql } from 'apollo-boost'
 
 const GET_BOOKS = gql`
   query Books {
-    books { id author title }
+    books { id author title completed }
   }
 `
 
 const ADD_BOOK = gql`
   mutation AddBook($title: String!, $author: String!) {
-    addBook(title: $title, author: $author) { id author title }
+    addBook(title: $title, author: $author) { id author title completed }
   }
 `
 
 const REMOVE_BOOK = gql`
   mutation RemoveBook($id: String!) {
-    removeBook(id: $id) { id author title }
+    removeBook(id: $id) { id author title completed }
+  }
+`
+
+const TOGGLE_COMPLETION = gql`
+  mutation ToggleCompletion($id: String!) {
+    toggleCompletion(id: $id) { id author title completed }
   }
 `
 
 export {
   GET_BOOKS,
   ADD_BOOK,
-  REMOVE_BOOK
+  REMOVE_BOOK,
+  TOGGLE_COMPLETION
 }
